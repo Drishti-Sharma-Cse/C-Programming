aa8549b05ad1999eb48f633dddb2433e880ec165
Drishti-Sharma-Cse <drishtii.sh08@gmail.com>
2026-07-17 23:30:50 +0530

    Added student marks program

---
+++ marks.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int m1,m2,m3;
+    printf("Enter your Maths marks");
+    scanf("%d",&m1);
+    printf("Enter your Physics marks");
+    scanf("%d",&m2);
+    printf("Enter your Chemistry marks");
+    scanf("%d",&m3);
+    printf( "Maths marks is %d\n",m1);
+    printf( "Physics marks is %d\n",m2);
+    printf( "Chemistry marks is %d\n",m3);
+    return 0;
+}
