1b4a8a3440d8aadd99ec93df77ee832c02219e87
Drishti-Sharma-Cse <drishtii.sh08@gmail.com>
2026-07-27 22:34:32 +0530

    Added leap year program

---
+++ leap_year.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void leapYear(int year){
+    if((year%400==0) && (year%4==0) || (year%100!=0)){
+        printf("Leap Year");
+    }else{
+        printf("Not a leap year");
+    }
+}
+int main(){
+    int year;
+    printf("Enter year");
+    scanf("%d",&year);
+    leapYear(year);
+    return 0;
+}
