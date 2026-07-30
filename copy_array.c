5510610bd37ace6a386f6d57a79259617857519f
Drishti-Sharma-Cse <drishtii.sh08@gmail.com>
2026-07-30 15:29:36 +0530

    Added copy array program

---
+++ copy_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr1[5], arr2[5], i;
+
+    printf("Enter 5 numbers:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+
+    for(i = 0; i < 5; i++)
+    {
+        arr2[i] = arr1[i];
+    }
+
+    printf("Copied Array:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d ", arr2[i]);
+    }
+
+    return 0;
+}
