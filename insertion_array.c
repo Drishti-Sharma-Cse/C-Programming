d35e3d024b62b54476f84db4e86228dbc7be9e2f
Drishti-Sharma-Cse <drishtii.sh08@gmail.com>
2026-07-31 16:49:38 +0530

    Added insertion array program

---
+++ insertion_array.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[10], i, n, pos, element;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter position (1 to %d): ", n + 1);
+    scanf("%d", &pos);
+
+    printf("Enter element to insert: ");
+    scanf("%d", &element);
+
+    for(i = n; i >= pos; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos - 1] = element;
+    n++;
+
+    printf("Array after insertion:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
