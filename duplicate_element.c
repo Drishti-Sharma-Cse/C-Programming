7c8e4eb3852e7ff81c2416a347736755537a851d
Drishti-Sharma-Cse <drishtii.sh08@gmail.com>
2026-07-31 16:43:38 +0530

    Added duplicate element array program

---
+++ duplicate_element.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5], i, j;
+
+    printf("Enter 5 numbers:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Duplicate Elements:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        for(j = i + 1; j < 5; j++)
+        {
+            if(arr[i] == arr[j])
+            {
+                printf("%d ", arr[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
