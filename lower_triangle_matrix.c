ed318b1aa0f842d0fccc26bd8adbbf5e1b2f8445
Drishti-Sharma-Cse <drishtii.sh08@gmail.com>
2026-08-03 16:37:10 +0530

    Added lower triangle matrix program

---
+++ lower_triangle_matrix.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[3][3];
+    int i, j;
+
+    printf("Enter 3x3 matrix:\n");
+
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    printf("Lower Triangle Elements:\n");
+
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            if(i >= j)
+                printf("%d ", arr[i][j]);
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
