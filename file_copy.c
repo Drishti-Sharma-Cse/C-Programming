77e826c91e658058112f6782ab137871f33d78bd
Drishti-Sharma-Cse <drishtii.sh08@gmail.com>
2026-08-09 21:58:55 +0530

    Added program to copy file

---
+++ file_copy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *source, *destination;
+    char ch;
+
+    source = fopen("Drishti.txt", "r");
+    destination = fopen("copy.txt", "w");
+
+    if(source == NULL || destination == NULL)
+    {
+        printf("File could not be opened");
+        return 0;
+    }
+
+    while((ch = fgetc(source)) != EOF)
+    {
+        fputc(ch, destination);
+    }
+
+    fclose(source);
+    fclose(destination);
+
+    printf("File copied successfully");
+
+    return 0;
+}
